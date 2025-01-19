f39e6f412c1fe4060ff848ed99c3cfcab56325fc
Nishant Mourya <mourya78645@gmail.com>
2025-01-19 17:15:12 +0530

    Dashboard changes 4

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ import reportWebVitals from './reportWebVitals';
 import 'aos/dist/aos.css'; // Import AOS styles
 import AOS from 'aos';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'; // Import React Query Devtools
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'; // Optional: React Query Devtools
 
-
+    
 // Create a new QueryClient instance with default options
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -51,7 +51,5 @@ const Root = () => {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Root />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Performance monitoring
 reportWebVitals();
